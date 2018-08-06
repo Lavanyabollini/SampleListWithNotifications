d990a1dc48890a1d5aff740009ba15d28b2fe89c
TailWebs <tailwebs@tailwebss-macbook-air-2.local>
2018-08-06 14:14:11 +0530

    Initial Commit

---
+++ SampleList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleList
+//
+//  Created by TailWebs on 06/08/18.
+//  Copyright © 2018 Tailwebs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
